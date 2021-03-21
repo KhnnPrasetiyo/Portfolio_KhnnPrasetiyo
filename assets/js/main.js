391a23f451a3beac0dc332a74e7e05f179aaebb2
KhnnPrasetiyo <markhanprasetiyo@gmail.com>
2021-03-21 13:39:15 +0700

    change title

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,7 +113,7 @@
 filterSelection("all") // Execute the function and show all columns
 function filterSelection(c) {
 	var x, i;
-	x = document.getElementsByClassName("column");
+	x = document.getElementsByClassName("colom");
 	if (c == "all") c = "";
 	// Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
 	for (i = 0; i < x.length; i++) {
